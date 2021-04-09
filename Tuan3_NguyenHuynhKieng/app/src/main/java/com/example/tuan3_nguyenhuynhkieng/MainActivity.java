46d9352abb7bd6d9f4cb5dd5943435244da44891
kienghuynh <63726724+kienghuynh@users.noreply.github.com>
2021-04-09 10:44:17 +0700

    first commit

---
+++ Tuan3_NguyenHuynhKieng/app/src/main/java/com/example/tuan3_nguyenhuynhkieng/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tuan3_nguyenhuynhkieng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
